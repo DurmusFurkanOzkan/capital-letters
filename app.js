2af91b4ca662770311626053ffde217c135999c1
Furkan <furkandfo8@gmail.com>
2023-06-10 14:15:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//* SELECTORS
+
+const textarea=document.querySelector("#textarea");
+
+const btn=document.querySelector("#btn");
+
+let par=document.createElement("p");
+
+btn.addEventListener("click", ()=>{
+    
+})
